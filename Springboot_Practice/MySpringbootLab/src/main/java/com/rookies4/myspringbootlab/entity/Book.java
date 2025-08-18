d0ca49e174fb392afba73fb8f61395c3a25fd15b
JIEUN <163732805+jxieun@users.noreply.github.com>
2025-08-18 13:04:28 +0900

    Book entity 수정

--- Springboot_Practice/MySpringbootLab/src/main/java/com/rookies4/myspringbootlab/entity/Book.java
+++ Springboot_Practice/MySpringbootLab/src/main/java/com/rookies4/myspringbootlab/entity/Book.java
@@ -3,17 +3,13 @@ package com.rookies4.myspringbootlab.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.DynamicUpdate;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "books")
 @Getter
 @Setter
-@DynamicUpdate
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,7 +18,7 @@ public class Book {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false) // unique = true 삭제
+    @Column(nullable = false)
     private String author;
 
     @Column(unique = true, nullable = false)
@@ -32,5 +28,5 @@ public class Book {
     private int price;
 
     @Column(nullable = false)
-    private LocalDate publishDate = LocalDate.now();
+    private LocalDate publishDate;
 }
