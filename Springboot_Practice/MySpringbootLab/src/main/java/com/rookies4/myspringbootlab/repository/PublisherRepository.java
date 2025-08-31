c8fd7c486be65336bff3fb8f7f94aeee6eefcd1b
JIEUN <163732805+jxieun@users.noreply.github.com>
2025-08-31 17:51:44 +0900

    PublisherRepository add

--- Springboot_Practice/MySpringbootLab/src/main/java/com/rookies4/myspringbootlab/repository/PublisherRepository.java
+++ Springboot_Practice/MySpringbootLab/src/main/java/com/rookies4/myspringbootlab/repository/PublisherRepository.java
@@ -1,4 +1,20 @@
 package com.rookies4.myspringbootlab.repository;
 
-public interface PublisherRepository {
+import com.rookies4.myspringbootlab.entity.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+
+    Optional<Publisher> findByName(String name);
+
+    @Query("SELECT d FROM Department d LEFT JOIN FETCH d.students WHERE d.id = :id")
+    Optional<Publisher> findByIdWithBooks(@Param("id") Long Id);
+
+    boolean existsByName(String name);
 }
